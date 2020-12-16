d152eed6f7c3c0ff1edf4c4fc1e42f1af76fb100
Teablack <ddaria.poda@gmail.com>
2020-12-16 19:24:40 +0100

    warunkowe uruchamianie (wstepnie)

--- src/main/java/pl/put/poznan/builder/rest/BootstrapBuilderController.java
+++ src/main/java/pl/put/poznan/builder/rest/BootstrapBuilderController.java
@@ -13,11 +13,17 @@ public class BootstrapBuilderController {
     public static String main(String header, Boolean footer,String title,String twitter_title,String og_title,String description,String twitter_description,String og_description,String keywords,String tw_linkObrazek,String card,String og_linkObrazek,String linkUrl){
         String w = "";
         w += new BootstrapBuilder(new Poczatek()).wywolanie(new Poczatek());
+
+        if((title!="") && (description!="") && (keywords!=""))
         w += new BootstrapBuilder(new Metatagi(title, description, keywords)).wywolanie(new Metatagi(title, description, keywords));
+        if((twitter_title!="") && (twitter_description!="") && (card!="") && (tw_linkObrazek!="") )
         w += new BootstrapBuilder(new MetaTwitter(twitter_title, twitter_description, tw_linkObrazek, card)).wywolanie(new MetaTwitter(twitter_title, twitter_description, tw_linkObrazek, card));
+        if((og_title!="") && (og_description!="") && (linkUrl!="") && (og_linkObrazek!="") )
         w += new BootstrapBuilder(new MetaOg(og_title, og_description, og_linkObrazek, linkUrl)).wywolanie(new MetaOg(og_title, og_description, og_linkObrazek, linkUrl));
         w += new BootstrapBuilder(new Srodek()).wywolanie(new Srodek());
+        if(header=="static")
         w += new BootstrapBuilder(new Nagłowek()).wywolanie(new Nagłowek());
+        if(header=="fixed")
         w += new BootstrapBuilder(new RuchomyNaglowek()).wywolanie(new RuchomyNaglowek());
         w += new BootstrapBuilder(new Obiekt()).wywolanie(new Obiekt());
         w += new BootstrapBuilder(new Koncowka()).wywolanie(new Koncowka());
@@ -31,19 +37,19 @@ public class BootstrapBuilderController {
             @RequestParam(value="header", defaultValue="fixed") String header, //fixed or static
             @RequestParam(value="footer", defaultValue="false") Boolean footer,
 
-            @RequestParam(value="title", defaultValue="title") String title,
-            @RequestParam(value="twitter_title", defaultValue="title") String twitter_title,
-            @RequestParam(value="og_title", defaultValue="title") String og_title,
+            @RequestParam(value="title", defaultValue="") String title,
+            @RequestParam(value="twitter_title", defaultValue="") String twitter_title,
+            @RequestParam(value="og_title", defaultValue="") String og_title,
 
-            @RequestParam(value="description", defaultValue="description") String description,
-            @RequestParam(value="twitter_description", defaultValue="description") String twitter_description,
-            @RequestParam(value="og_description", defaultValue="description") String og_description,
+            @RequestParam(value="description", defaultValue="") String description,
+            @RequestParam(value="twitter_description", defaultValue="") String twitter_description,
+            @RequestParam(value="og_description", defaultValue="") String og_description,
 
-            @RequestParam(value="keywords", defaultValue="keywords") String keywords,
-            @RequestParam(value="tw_linkObrazek", defaultValue="linkObrazek") String tw_linkObrazek,
-            @RequestParam(value="card", defaultValue="card") String card,
-            @RequestParam(value="og_linkObrazek", defaultValue="og_linkObrazek") String og_linkObrazek,
-            @RequestParam(value="linkUrl", defaultValue="linkUrl") String linkUrl
+            @RequestParam(value="keywords", defaultValue="") String keywords,
+            @RequestParam(value="tw_linkObrazek", defaultValue="") String tw_linkObrazek,
+            @RequestParam(value="card", defaultValue="") String card,
+            @RequestParam(value="og_linkObrazek", defaultValue="") String og_linkObrazek,
+            @RequestParam(value="linkUrl", defaultValue="") String linkUrl
     ){
 
         // log the parameters
